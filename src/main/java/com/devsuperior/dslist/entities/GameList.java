1a776c9c10f15cf6bb3c00086b9a05cc3c6f40a5
Marcos1608 <marcosluis1608@gmail.com>
2025-08-24 21:33:04 -0300

    GET lists

--- src/main/java/com/devsuperior/dslist/entities/GameList.java
+++ src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -34,11 +34,11 @@ public class GameList {
 		this.id = id;
 	}
 
-	public String getNome() {
+	public String getName() {
 		return name;
 	}
 
-	public void setNome(String nome) {
+	public void setName(String nome) {
 		this.name = nome;
 	}
 
